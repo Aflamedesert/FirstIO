c25dcad9e9b253db27132187e2efb90107e42f60
Aflamedesert <Michael.w.crowe@gmail.com>
2018-09-05 16:53:16 -0700

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,9 @@
+//Author: Michael
 #include <iostream>
 #include <string>
-
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is the boi friedricht, what is yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
